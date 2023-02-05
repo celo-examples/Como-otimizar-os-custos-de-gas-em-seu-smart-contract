d1170acc26bc18f10ec6c5a08c2d2aebbc3d3a44
Gustavo <gustavo@obex.pw>
2023-02-05 13:54:17 -0300

    fix russian roulette deploy script

--- russian-roulette-solidity/scripts/deploy.ts
+++ russian-roulette-solidity/scripts/deploy.ts
@@ -1,18 +1,13 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const RussianRouletteFactory = await ethers.getContractFactory("RussianRoulette");
+  const RussianRoulette = await RussianRouletteFactory.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await RussianRoulette.deployed();
 
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`RussianRoulette deployed to ${RussianRoulette.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
